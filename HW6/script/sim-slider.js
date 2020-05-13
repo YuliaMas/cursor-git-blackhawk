842f69968b1adbf8ac295730b2fdbd8fa9eda667
Yuliya <yulia.masechko@gmail.com>
2020-05-13 23:23:23 +0300

    change HW6 , add styles

--- HW6/script/sim-slider.js
+++ HW6/script/sim-slider.js
@@ -5,8 +5,8 @@ function Sim(sldrId) {
 		this.sldrRoot = id
 	}
 	else {
-		this.sldrRoot = document.querySelector('.sim-slider')
-	};
+		this.sldrRoot = document.querySelector('.sim-slider');
+	}
 
 	// Slider objects
 	this.sldrList = this.sldrRoot.querySelector('.sim-slider-list');
@@ -19,7 +19,7 @@ function Sim(sldrId) {
 	// Initialization
 	this.options = Sim.defaults;
 	Sim.initialize(this)
-};
+}
 
 Sim.defaults = {
 
@@ -29,7 +29,7 @@ Sim.defaults = {
 	interval: 5000, // Интервал между пролистыванием элементов (мс)
 	arrows: true,   // Пролистывание стрелками
 	dots: true      // Индикаторные точки
-};
+}
 
 Sim.prototype.elemPrev = function(num) {
 	num = num || 1;
@@ -39,19 +39,19 @@ Sim.prototype.elemPrev = function(num) {
 	if(this.currentElement < 0) this.currentElement = this.elemCount-1;
 
 	if(!this.options.loop) {
-		if(this.currentElement == 0) {
-			this.leftArrow.style.display = 'none'
-		};
-		this.rightArrow.style.display = 'block'
-	};
+		if(this.currentElement === 0) {
+			this.leftArrow.style.display = 'none';
+		}
+		this.rightArrow.style.display = 'block';
+	}
 
 	this.sldrElements[this.currentElement].style.opacity = '1';
 	this.sldrElements[prevElement].style.opacity = '0';
 
 	if(this.options.dots) {
-		this.dotOn(prevElement); this.dotOff(this.currentElement)
+		this.dotOn(prevElement); this.dotOff(this.currentElement);
 	}
-};
+}
 
 Sim.prototype.elemNext = function(num) {
 	num = num || 1;
@@ -61,33 +61,29 @@ Sim.prototype.elemNext = function(num) {
 	if(this.currentElement >= this.elemCount) this.currentElement = 0;
 
 	if(!this.options.loop) {
-		if(this.currentElement == this.elemCount-1) {
-			this.rightArrow.style.display = 'none'
-		};
-		this.leftArrow.style.display = 'block'
-	};
+		if(this.currentElement === this.elemCount-1) {
+			this.rightArrow.style.display = 'none';
+		}
+		this.leftArrow.style.display = 'block' ;
+	}
 
 	this.sldrElements[this.currentElement].style.opacity = '1';
 	this.sldrElements[prevElement].style.opacity = '0';
 
 	if(this.options.dots) {
-		this.dotOn(prevElement); this.dotOff(this.currentElement)
+		this.dotOn(prevElement); this.dotOff(this.currentElement) ;
 	}
-};
+}
 
 Sim.prototype.dotOn = function(num) {
 	this.indicatorDotsAll[num].style.cssText =
-			'background-color:#FFF; cursor:pointer;'
-};
+			'background-color:#FFF; cursor:pointer;' ;
+}
 
 Sim.prototype.dotOff = function(num) {
 	this.indicatorDotsAll[num].style.cssText =
-			'background-color:#F063B8;' +
-			' width: 10px; ' +
-			'height:10px; ' +
-			// ''     вирівняти рожеву точку
-			' cursor: default ;'
-};
+			`background-color:#F063B8; width: 10px; height: 10px;  cursor: default ;` ;
+}
 
 Sim.initialize = function(that) {
 
@@ -101,26 +97,27 @@ Sim.initialize = function(that) {
 	// Functions
 	function getTime() {
 		return new Date().getTime();
-	};
+	}
 	function setAutoScroll() {
 		that.autoScroll = setInterval(function() {
 			let fnTime = getTime();
 			if(fnTime - bgTime + 10 > that.options.interval) {
-				bgTime = fnTime; that.elemNext()
+				bgTime = fnTime;
+				that.elemNext();
 			}
-		}, that.options.interval)
-	};
+		}, that.options.interval);
+	}
 
 	// Start initialization
 	if(that.elemCount <= 1) {   // Отключить навигацию
 		that.options.auto = false;
 		that.options.arrows = false; that.options.dots = false;
 		that.leftArrow.style.display = 'none';
-		that.rightArrow.style.display = 'none'
-	};
+		that.rightArrow.style.display = 'none';
+	}
 	if(that.elemCount >= 1) {   // показать первый элемент
 		that.sldrElemFirst.style.opacity = '1';
-	};
+	}
 
 	if(!that.options.loop) {
 		that.leftArrow.style.display = 'none';  // отключить левую стрелку
@@ -132,8 +129,8 @@ Sim.initialize = function(that) {
 		that.sldrList.addEventListener('mouseenter', function() {
 			clearInterval(that.autoScroll)
 		}, false);
-		that.sldrList.addEventListener('mouseleave', setAutoScroll, false)
-	};
+		that.sldrList.addEventListener('mouseleave', setAutoScroll, false);
+	}
 
 	if(that.options.arrows) {  // инициализация стрелок
 		that.leftArrow.addEventListener('click', function() {
@@ -145,20 +142,20 @@ Sim.initialize = function(that) {
 		that.rightArrow.addEventListener('click', function() {
 			let fnTime = getTime();
 			if(fnTime - bgTime > 1000) {
-				bgTime = fnTime; that.elemNext()
+				bgTime = fnTime; that.elemNext();
 			}
-		}, false)
+		}, false);
 	}
 	else {
 		that.leftArrow.style.display = 'none';
-		that.rightArrow.style.display = 'none'
-	};
+		that.rightArrow.style.display = 'none';
+	}
 
 	if(that.options.dots) {  // инициализация индикаторных точек
 		let sum = '', diffNum;
 		for(let i=0; i<that.elemCount; i++) {
-			sum += '<span class="sim-dot"></span>'
-		};
+			sum += '<span class="sim-dot"></span>';
+		}
 		that.indicatorDots.innerHTML = sum;
 		that.indicatorDotsAll =
 				that.sldrRoot.querySelectorAll('span.sim-dot');
@@ -172,12 +169,12 @@ Sim.initialize = function(that) {
 				else if(n > that.currentElement) {
 					bgTime = getTime(); that.elemNext(diffNum)
 				}
-				// Если n == that.currentElement ничего не делаем
-			}, false)
-		};
+				// Если n === that.currentElement ничего не делаем
+			}, false);
+		}
 		that.dotOff(0);  // точка[0] выключена, остальные включены
 		for(let i=1; i<that.elemCount; i++) {
-			that.dotOn(i)
+			that.dotOn(i);
 		}
 	}
 };
