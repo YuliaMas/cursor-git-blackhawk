b2ec10f0b0188b5ec854685ea580392955b0b56f
Yuliya <yulia.masechko@gmail.com>
2020-06-14 13:22:24 +0300

    modifier files

--- HW14/js/main.js
+++ HW14/js/main.js
@@ -1,9 +1,7 @@
 $(document.body).append(`
-<!--        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>-->
-<!--        <link rel="stylesheet" href="../css/main.css">-->
-        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-        <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-    `);
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" async defer></script>
+`);
 $(window).on("load", () => {
   console.log("on load");
   $(".preloader").fadeOut(200);
