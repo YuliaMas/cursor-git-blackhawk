8a331b1f22de74b1c388860902a0c071070c0204
Yuliya <yulia.masechko@gmail.com>
2020-06-14 09:19:20 +0300

    add slider

--- HW14/js/main.js
+++ HW14/js/main.js
@@ -1,3 +1,14 @@
-'use strict';
-
-console.log('test');
+$(document.body).append(`
+        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<!--        <link rel="stylesheet" href="../css/main.css">-->
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    `);
+$(window).on("load", () => {
+  console.log("on load");
+  $(".preloader").fadeOut(200);
+  $(".review-slider").slick({
+    prevArrow: $(".arrow-left"),
+    nextArrow: $(".arrow-right"),
+  });
+});
