8f1585de9250def2938a4c5debf4666a8cbc6df2
Yuliya <yulia.masechko@gmail.com>
2020-07-11 12:27:09 +0300

    add media and burger menu

--- general_folder/js/main.js
+++ general_folder/js/main.js
@@ -21,3 +21,23 @@ function up() {
   } else clearTimeout(t);
   return false;
 }
+
+(function () {
+  let hamburger = {
+    nav: document.querySelector("#nav"),
+    navToggle: document.querySelector(".nav-toggle"),
+
+    initialize() {
+      this.navToggle.addEventListener("click", () => {
+        this.toggle();
+      });
+    },
+
+    toggle() {
+      this.navToggle.classList.toggle("expanded");
+      this.nav.classList.toggle("expanded");
+    },
+  };
+
+  hamburger.initialize();
+})();
