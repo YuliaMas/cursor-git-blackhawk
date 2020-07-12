42e6fca376c36fae54b39b798caa6bd7ec72c80b
Yuliya <yulia.masechko@gmail.com>
2020-07-13 01:43:50 +0300

    add animation images

--- general_folder/js/main.js
+++ general_folder/js/main.js
@@ -41,3 +41,83 @@ function up() {
 
   hamburger.initialize();
 })();
+
+// random img
+// sha = function () {
+//   return Math.floor(Math.random() * 2);
+// };
+// sha = function getImage(myImg) {
+//   const random = Math.floor(Math.random() * 2);
+//   console.log(random);
+//   return '<img src="img/' + random + '.png" border="0" />';
+// };
+//
+// document.getElementById("im1").innerHTML = myImg;
+// console.log(myImg);
+
+// let arr = [];
+// let name = "im";
+// function nameId() {
+//   for (let i = 1; i < 9; i++) {
+//     name = name + i;
+//     const arrName = arr.push(name);
+//     console.log(arrName);
+//     return arrName;
+//   }
+// }
+// console.log(arr);
+// nameId();
+
+// add id for array
+let arr = [];
+function nameId(n) {
+  for (let i = 1; i < n; i++) {
+    let idName = "im" + i;
+    //console.log(idName);
+    arr.push(idName);
+    //console.log(arr);
+  }
+  return arr;
+}
+nameId(15);
+// console.log(arr);
+
+// choose one element (id) from array
+function randomF() {
+  // for (let i = 0; i < arr.length; i++) {
+  let idNameRandom = Math.floor(Math.random() * arr.length);
+  let array = arr[idNameRandom];
+  let elem = document.getElementById(array);
+  //console.log(array);
+  //console.log(elem);
+  return elem;
+  // }
+}
+
+function prevSize(image) {
+  image.style.width = " calc(15vw - 4px) ";
+  image.style.height = " calc(15vh - 4px ) ";
+
+  setTimeout(changeSize, 1000);
+}
+
+function changeSize() {
+  let image = randomF();
+  image.style.width = "calc(20vw - 5px)";
+  image.style.height = "calc(20vh - 3px)";
+
+  setTimeout(prevSize, 1000, image);
+}
+changeSize();
+
+//
+// function myFunction() {
+//   var element = document.getElementById("myDIV");
+//   element.classList.add("mystyle");
+// }
+//
+// function myFunction() {
+//   var element = document.getElementById("myDIV");
+//   element.classList.remove("mystyle");
+// }
+//
