868ebdd3d63aa5f9d96f0fd537e08df81935e9ac
Yuliya <yulia.masechko@gmail.com>
2020-07-17 18:30:20 +0300

    add add function and canvas

--- hwm-two-face/hw2-advanced/js/main.js
+++ hwm-two-face/hw2-advanced/js/main.js
@@ -1,5 +1,6 @@
 const getNumN = document.getElementById("numN");
 const getNumM = document.getElementById("numM");
+const getSum = document.querySelector("#sum");
 
 const checkNum = (char) => {
   let num = null;
@@ -10,11 +11,59 @@ const checkNum = (char) => {
   return num;
 };
 
-N = checkNum("N");
-M = checkNum("M");
+render = () => {
+  getNumN.textContent = N;
+  getNumM.textContent = M;
+};
 
-let a = confirm("Пропускати парні числа чи ні?");
-alert(a);
+const N = checkNum("N");
+const M = checkNum("M");
 
-getNumN.textContent = N;
-getNumM.textContent = M;
+render();
+
+const minN = Math.min(M, N);
+const maxM = Math.max(M, N);
+
+let isEven = confirm("Пропускати парні числа чи ні?");
+alert(isEven);
+
+let sumNumbers;
+
+if (!isEven) {
+  alert(sumTo(maxM));
+} else if (isEven && minN % 2 === 0) {
+  alert(sumNtoM(maxM));
+} else if (isEven && minN % 2 !== 0) {
+  alert(sumOdd(maxM));
+}
+
+function sumTo(maxM) {
+  let sum = 0;
+  for (let i = minN; i <= maxM; i++) {
+    sum += i;
+  }
+  // return sum;
+  sumNumbers = sum;
+  return sumNumbers;
+}
+
+function sumNtoM(maxM) {
+  let sum = 0;
+  for (let i = minN + 1; i <= maxM; i += 2) {
+    sum += i;
+  }
+  // return sum;
+  sumNumbers = sum;
+  return sumNumbers;
+}
+
+function sumOdd(maxM) {
+  let sum = 0;
+  for (let i = minN; i <= maxM; i += 2) {
+    sum += i;
+  }
+  sumNumbers = sum;
+  return sumNumbers;
+}
+
+getSum.textContent = sumNumbers;
