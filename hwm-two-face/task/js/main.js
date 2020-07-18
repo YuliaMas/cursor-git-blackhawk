8a908b0fdb9d541f197fb79469ad585d1ac4cbfe
Yuliya <yulia.masechko@gmail.com>
2020-07-19 00:49:25 +0300

    modified 5 task

--- hwm-two-face/task/js/main.js
+++ hwm-two-face/task/js/main.js
@@ -2,45 +2,52 @@
 // залежності від числа буде виводити слово (Наприклад, якщо n дорівнює 3, то буде виводитися слово «три»)
 const n = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-for (let i = 0; i < n.length; i++) {
-  const randomNumber = Math.floor(Math.random() * n[i]);
-
-  switch (randomNumber) {
-    case 0:
-      alert("0 - zero");
-      break;
-    case 1:
-      alert("1 - one");
-      break;
-    case 2:
-      alert("2 - two");
-      break;
-    case 3:
-      alert("3 - three");
-      break;
-    case 4:
-      alert("4 - four");
-      break;
-    case 5:
-      alert("5 - five");
-      break;
-    case 6:
-      alert("6 - six");
-      break;
-    case 7:
-      alert("7 - seven");
-      break;
-    case 8:
-      alert("8 - eight");
-      break;
-    case 9:
-      alert("9 - nine");
-      break;
-    default:
-      alert("number");
-      break;
-  }
+// for (let i = 0; i < n.length; i++) {
+// const randomNumber = Math.floor(Math.random() * n[i]);
+let number = "";
+const getNumber = () => {
+  number = prompt("Введіть ваше число від 0 до 9", "");
+  return number;
+};
+getNumber();
+// switch (randomNumber) {
+switch (number) {
+  case "0":
+    alert("0 - zero");
+    break;
+  case "1":
+    alert("1 - one");
+    break;
+  case "2":
+    alert("2 - two");
+    break;
+  case "3":
+    alert("3 - three");
+    break;
+  case "4":
+    alert("4 - four");
+    break;
+  case "5":
+    alert("5 - five");
+    break;
+  case "6":
+    alert("6 - six");
+    break;
+  case "7":
+    alert("7 - seven");
+    break;
+  case "8":
+    alert("8 - eight");
+    break;
+  case "9":
+    alert("9 - nine");
+    break;
+  default:
+    alert(" Це не число! ");
+    getNumber();
+    break;
 }
+// }
 
 //2.	Використовуючи document.write () і будь-яку з циклічних конструкцій виведіть десять
 // однакових зображень (треба виводити <img src = "..." alt = "..." />)
@@ -50,15 +57,15 @@ for (let i = 0; i < n.length; i++) {
 // }
 
 const container = document.createDocumentFragment();
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 12; i++) {
   const img = document.createElement("img");
   container.appendChild(img);
-  // let img = div.appendChild(document.createElement("img"));
-  img.style.width = "300";
+  // img.style.width = "300";
   img.src = "img/berry.png";
   img.alt = "berries";
 }
 document.body.appendChild(container);
+document.write("<br />");
 
 // 5.Скоротіть код використовуючи цикли:
 // <Script type = 'text / javascript'>
@@ -78,12 +85,27 @@ document.body.appendChild(container);
 // document.write ( '<br />');
 // document.write ( 'Число: <b> 0 </ b>');
 // </ Script>
-document.write("<br />");
-const arr = [100, 80, 60, 50, 40, 20, 10, 0];
-for (let i = 0; i <= arr.length - 1; i++) {
-  document.write(`Число: <b> ${arr[i]} </b>`);
-  document.write("<br />");
+
+for (let i = 100; i >= 0; i = i - 20) {
+  document.write(`Число: <b> ${i} </b> <br />`);
+  switch (i) {
+    case 60:
+    case 50:
+    case 20:
+    case 10:
+      i = i + 10;
+      break;
+  }
+  // if (i === 60 || i === 50 || i === 20 || i === 10) {
+  //   i = i + 10;
+  // }
 }
+
+// const arr = [100, 80, 60, 50, 40, 20, 10, 0];
+// for (let i = 0; i <= arr.length - 1; i++) {
+//   document.write(`Число: <b> ${arr[i]} </b>`);
+//   document.write("<br />");
+// }
 
 // 3.	Сформувати для учня список складається з n елементів, кожен елемент якого - це оцінка від 2 до 5.
 const arrayPupil = [];
@@ -130,14 +152,10 @@ const chessF = () => {
   for (let j = 1; j < 10; j++) {
     switch (j % 2) {
       case 0:
-        // a = "#";
-        // b = "0";
         charFirst = "#";
         charSecond = "⠀";
         break;
       case 1:
-        // a = "0";
-        // b = "#";
         charFirst = "⠀";
         charSecond = "#";
         break;
