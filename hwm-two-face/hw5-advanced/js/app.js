e4410728942e15ba20ca7d20110d123276b8ed45
Yuliya <yulia.masechko@gmail.com>
2020-07-29 09:06:33 +0300

    add task 1-9 and styles

--- hwm-two-face/hw5-advanced/js/app.js
+++ hwm-two-face/hw5-advanced/js/app.js
@@ -1,28 +1,55 @@
-// 1. Создайте функцию getRandomArray(length, min, max) – которая возвращает массив случайных целых чисел.
-//		В функции есть параметры: length - длина массива, min – минимальное значение целого числа, max – максимальное
-//		значение целого числа. Пример: getRandomArray(15, 1, 100) –> [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2]
-
+// 1. Функція повертає масив випадкових  цілих чисел.
 function getRandomArray(length, min, max) {
   const randomArr = [];
-  if (length <= 0) {
-    return "Введіть  ціле додатне число в полі довжина масиву";
-  }
   for (let i = 0; i < length; i++) {
     const randomNum = Math.floor(Math.random() * (max + 1 - min) + min);
     randomArr.push(randomNum);
-    console.log(randomNum);
   }
   return randomArr;
-  // return randomArr.join(" , ").split(",");
 }
 
-// 3.  Создайте функцию getAverage(...numbers) – которая считает среднее арифметическое всех переданных в неё аргументов. НЕЦЕЛЫЕ ЧИСЛА ИГНОРИРУЮТСЯ
-// Пример: getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2) –> 34.4
+// 2. Функція обчислює моду всіх переданих в неї аргументів.
+function getModa(numbers) {
+  const arr = numbers;
+  const result = {};
+  arr.forEach(function (acc) {
+    // рахує кількість повторень
+    if (result[acc] !== undefined) ++result[acc];
+    else result[acc] = 1;
+  });
+  const res = Object.entries(result); // створює об'єкт і виводить пари значень
+  res.forEach((entry) => {
+    const key = entry[0];
+    const value = entry[1];
+    return `${key}: ${value}`;
+  });
+  // сортує в об'єкті value
+  let max = res.sort((a, b) => {
+    return a[1] - b[1];
+  });
+  let modaLast = max[max.length - 1][1];
+  // знаходить моду
+  let moda = [];
+  for (let i = 0; i < max.length; i++) {
+    if (max[i][1] === modaLast) {
+      moda.push(max[i][0]);
+    }
+  }
+  return ` moda: ${moda.join(" , ")}`;
+}
 
+// 3.  Функція рахує середнє арифметичне всіх переданих в неї аргументів.
 function getAverage(numbers) {
-  let num = numbers;
-  // const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
+  const num = numbers;
+  const result = num.reduce(function (sum, current) {
+    return sum + current;
+  }, 0);
+  const average = result / num.length;
+  if (isNaN(average)) {
+    return " В 1-му завданні помилка, виправте , будь ласка!";
+  }
+  return `Середнє арифметичне значення: ${average.toFixed(2)}`;
+  // const reducer = (accumulator, currentValue) => accumulator + currentValue;   // 1-ий варіант
   // for (let num of numArr) {
   // let num = 0;
   // let i = 0;
@@ -34,9 +61,68 @@ function getAverage(numbers) {
   //   (accumulator, currentValue) => accumulator + currentValue,
   //   0
   // );
-  let result = num.reduce(function (sum, current) {
-    return sum + current;
-  }, 0);
-  const average = result / num.length;
-  return average;
 }
+
+// 4. Функція рахує медіану.
+function getMedian(numbers) {
+  const medianEven = Math.floor((numbers.length - 1) / 2);
+  const medianOdd = Math.floor(numbers.length / 2);
+  numbers.sort(function (a, b) {
+    return a - b;
+  });
+  return numbers.length % 2 === 1
+    ? "Mедіана: " + numbers[medianOdd]
+    : "Mедіана: " + 0.5 * (numbers[medianEven] + numbers[medianEven + 1]);
+}
+
+//5. Функцію  фільтрує парні числа
+function filterEvenNumbers(numbers) {
+  const numOdd = numbers.filter((item) => item % 2).join(" , ");
+  return `Непарні числа: ${numOdd}`;
+}
+
+// 6. Функція рахує кількість чисел більших 0
+function countPositiveNumbers(numbers) {
+  const positiveNum = numbers.filter((item) => {
+    return item > 0;
+  });
+  return "Кількість додатних чисел: " + positiveNum.length;
+}
+
+// 7. Функція  відфільтровує усі елементи в масиві та залишає ті,що діляться на ціло на 5.
+// Приклад: getDividedByFive(6, 2, 55, 11, 50, 2, 55, 40, 57, 87, 23, 5, 56, 3, 2) -> [55, 55]
+function getDividedByFive(numbers) {
+  const dividedByFive = numbers.filter((item) => {
+    return !(item % 5);
+  });
+  return `Числа кратні 5: [ ${dividedByFive.join(" , ")} ]`;
+}
+
+// 8. Функція замінює погані слова на зірочки (*).
+function replaceBadWords(string) {
+  const regex = /(shit|fuck)/gi;
+  const newStr = string.replace(regex, "****");
+  return `Модифіковане речення: ${newStr}`;
+}
+
+// 9. Функція розбиває кожне слово на умовні склади по 3 букви.
+function divideByThree(word) {
+  if (word.length < 3) return word;
+  word = word.toLowerCase().split(" ").join("");
+  const arrDivideW = [];
+  for (let i = 0; i < word.length; i += 3) {
+    const divWord = word.substr(i, 3);
+    arrDivideW.push(divWord);
+  }
+  return `Розбите слово:  ${arrDivideW}`;
+}
+
+// 10. Створіть функцію generateCombinations(word), яка видасть всі можливі перестановки(унікальні, без повторень)
+// букв в слові. Для тестів не передавайте слова в яких більше 10 букв. Краще взагалі обмежити работу функції 10 буквами.
+// Приклад: generateCombinations("man") -> ["man", "mna", "amn", "anm", "nam", "nma"] Приклад: generateCombinations("ol")
+// -> ["ol", "lo"]
+function generateCombinations(word) {
+  let str = word.toLowerCase();
+  if (word.length > 10) return "vveditb slovo menwoi dovgwunu";
+}
+generateCombinations("man");
--- hwm-two-face/hw5-advanced/js/main.js
+++ hwm-two-face/hw5-advanced/js/main.js
@@ -1,21 +1,25 @@
 window.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("container");
+
   for (let i = 1; i <= 10; i++) {
     const el = document.createElement("div");
+    const pTask = document.createElement("p");
+    const button = document.createElement("button");
+    const p = document.createElement("p");
     el.className = "card";
     el.id = "card" + i;
     el.innerHTML = "Завдання" + " " + i;
+    el.append(pTask);
+    pTask.className = "task" + i;
     container.append(el);
-    const button = document.createElement("button");
     button.className = "start" + i;
     el.append(button);
-    button.textContent = "Натисни мене";
-    const p = document.createElement("p");
+    button.textContent = "Виконати";
     el.append(p);
     p.className = "answer" + i;
   }
 
-  function createInput(name, num, type) {
+  function createInput(name, num, typeName) {
     const div = document.createElement("div");
     name.append(div);
     for (let j = 1; j <= num; j++) {
@@ -24,21 +28,55 @@ window.addEventListener("DOMContentLoaded", () => {
       div.append(span);
       div.append(input);
       input.className = "field" + j;
-      input.type = type;
+      input.type = typeName;
       input.value = "";
+      input.required = true;
     }
   }
 
+  function createTextarea(name, idName, text) {
+    const div = document.createElement("div");
+    name.append(div);
+    const textarea = document.createElement("textarea");
+    const span = document.createElement("span");
+    div.append(span);
+    div.append(textarea);
+    textarea.id = idName;
+    textarea.required = true;
+    textarea.placeholder = text;
+  }
+  const getText = "Are you fucking kidding? It's bullshit! Holy shit!";
+  const div3Let = "Commander , Live ";
   createInput(card1, 3, "number");
-  createInput(card8, 2, "text");
-  const spans = document.getElementsByTagName("span");
-  // for (let i = 0; i < spans.length; i++) {
-  spans[0].textContent = "Введіть min значенння";
-  spans[1].innerHTML = "Введіть max значення";
-  spans[2].innerHTML = "Введіть довжину масиву";
-  // }
+  createTextarea(card8, "badWord", getText);
+  createTextarea(card9, "divWord", div3Let);
 
-  let num = [];
+  // const span = document.createElement("span");
+  // div.append(span);
+  // for (let i = 0; i < spans.length; i++) {
+  const spans = document.getElementsByTagName("span");
+  const pTask = "Масив значень дивись в 1-му завданні:";
+  const pTask2 = document.querySelector(".task2");
+  const pTask3 = document.querySelector(".task3");
+  const pTask4 = document.querySelector(".task4");
+  const pTask5 = document.querySelector(".task5");
+  const pTask6 = document.querySelector(".task6");
+  const pTask7 = document.querySelector(".task7");
+  const pTask8 = document.querySelector(".task8");
+  spans[0].textContent = "Введіть min значенння:";
+  spans[1].innerHTML = "Введіть max значення:";
+  spans[2].innerHTML = "Введіть довжину масиву:";
+  spans[3].innerHTML = "Введіть речення, яке містить слова 'shit' і/чи 'fuck':";
+  spans[4].innerHTML = "Введіть слово:";
+  pTask2.textContent = pTask;
+  pTask3.textContent = pTask;
+  pTask4.textContent = pTask;
+  pTask5.textContent = pTask;
+  pTask6.textContent = pTask;
+  pTask7.textContent = pTask;
+
+  /* - - - - - - - - - - - - - - Tasks - - - - - - - - - - - - - - - - - - - - - - -*/
+  let num;
   function task1() {
     const min = document.querySelector(".field1").value;
     const max = document.querySelector(".field2").value;
@@ -46,17 +84,62 @@ window.addEventListener("DOMContentLoaded", () => {
     const randomNumber = document.querySelector(".answer1");
     const numbers = getRandomArray(length, +min, +max);
     num = numbers;
+    if (max < min) {
+      return (randomNumber.textContent = `Введіть більше число в полі max`);
+    }
+    if (length <= 0) {
+      return (randomNumber.textContent =
+        "Введіть  ціле додатне число в полі довжина масиву");
+    }
     // numbers.split(" ");
-    randomNumber.textContent = `[ ${numbers} ]`;
+    randomNumber.textContent = `[ ${numbers.join(" , ")} ]`;
     // numbers = `${numbers} `;
     return num;
   }
 
   function task2() {
-    // const numbersArr = task1();
-    const number = document.querySelector(".answer2");
-    number.textContent = `${getAverage(num, length)}`;
+    const moda = document.querySelector(".answer2");
+    moda.textContent = `${getModa(num)}`;
   }
+
+  function task3() {
+    const number = document.querySelector(".answer3");
+    number.textContent = `${getAverage(num)}`;
+  }
+
+  function task4() {
+    const median = document.querySelector(".answer4");
+    median.textContent = `${getMedian(num)}`;
+  }
+
+  function task5() {
+    const filterEven = document.querySelector(".answer5");
+    filterEven.textContent = `${filterEvenNumbers(num)}`;
+  }
+
+  function task6() {
+    const filterEven = document.querySelector(".answer6");
+    filterEven.textContent = `${countPositiveNumbers(num)}`;
+  }
+
+  function task7() {
+    const filterEven = document.querySelector(".answer7");
+    filterEven.textContent = `${getDividedByFive(num)}`;
+  }
+
+  function task8() {
+    const text = document.querySelector("#badWord").value;
+    const setBadWords = document.querySelector(".answer8");
+    setBadWords.textContent = `${replaceBadWords(text)}`;
+  }
+
+  function task9() {
+    const text = document.querySelector("#divWord").value;
+    const setWord = document.querySelector(".answer9");
+    setWord.textContent = `${divideByThree(text)}`;
+  }
+
+  /* - - - - - - - - - - - - - - Buttons click - - - - - - - - - - - - - -*/
 
   document.body.addEventListener("click", (event) => {
     if (event.target.classList.contains("start1")) {
@@ -67,6 +150,48 @@ window.addEventListener("DOMContentLoaded", () => {
   document.body.addEventListener("click", (event) => {
     if (event.target.classList.contains("start2")) {
       `${task2()}`;
+    }
+  });
+
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("start3")) {
+      `${task3()}`;
+    }
+  });
+
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("start4")) {
+      `${task4()}`;
+    }
+  });
+
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("start5")) {
+      `${task5()}`;
+    }
+  });
+
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("start6")) {
+      `${task6()}`;
+    }
+  });
+
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("start7")) {
+      `${task7()}`;
+    }
+  });
+
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("start8")) {
+      `${task8()}`;
+    }
+  });
+
+  document.body.addEventListener("click", (event) => {
+    if (event.target.classList.contains("start9")) {
+      `${task9()}`;
     }
   });
 });
