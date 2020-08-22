f24eaad17c49a5274b82ac2040ed800a57a08910
Yuliya <yulia.masechko@gmail.com>
2020-08-22 21:26:12 +0300

    add random phrase

--- hwm-two-face/hw11-advanced/js/app.js
+++ hwm-two-face/hw11-advanced/js/app.js
@@ -5,9 +5,13 @@ function setResult() {
       const list = document.getElementById("list");
       const text = document.getElementById("text");
       const time = document.getElementById("time");
+      const randomPhrase = document.getElementById("phraseRandom");
       list.innerHTML = `Translate:  ${signUp}`;
       text.innerHTML = `Result: ${result}`;
       time.innerHTML = `Runtime: ${data}ms`;
+      randomPhrase.innerHTML = `Phrase of the day: <span> ${random(
+        phrase
+      )} </span>`;
     })
     .catch(() => {
       text.innerHTML = "The number is smaller";
--- hwm-two-face/hw11-advanced/js/index.js
+++ hwm-two-face/hw11-advanced/js/index.js
@@ -2,6 +2,19 @@ const min = 10000;
 let data;
 let result = "";
 let signUp = "";
+const phrase = [
+  "Before I finish - 在我结束",
+  "We'd better stop now - 我们最好别看",
+  "I have attempted to explain here that... - 我试图在这里解释一下...",
+  "That remains to be seen - 这还有待",
+  "We agreed that... - ...我们同意",
+  "It makes sense that... - ...是有通理的",
+  "You are not alone - 你不是孤单一个人",
+];
+
+function random(array) {
+  return array[Math.floor(Math.random() * array.length) | 0];
+}
 
 async function doPromise(length) {
   let res = "";
