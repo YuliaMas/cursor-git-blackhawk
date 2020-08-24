2964923a258449a69faa1437755285f83c1fa95a
Yuliya <yulia.masechko@gmail.com>
2020-08-24 10:50:27 +0300

    add new variable chars

--- hwm-two-face/hw11-advanced/js/app.js
+++ hwm-two-face/hw11-advanced/js/app.js
@@ -1,18 +1,17 @@
 const setResult = async () => {
   const length = document.getElementById("length").value;
   try {
-    await getChars(+length).then((value) => {
-      const list = document.getElementById("list");
-      const text = document.getElementById("text");
-      const time = document.getElementById("time");
-      const randomPhrase = document.getElementById("phraseRandom");
-      list.innerHTML = `Translate:  ${signUp}`;
-      text.innerHTML = `Result: ${value}`;
-      time.innerHTML = `Runtime: ${data}ms`;
-      randomPhrase.innerHTML = `Phrase of the day: <span> ${random(
-        phrase
-      )} </span>`;
-    });
+    const chars = await getChars(+length);
+    const list = document.getElementById("list");
+    const text = document.getElementById("text");
+    const time = document.getElementById("time");
+    const randomPhrase = document.getElementById("phraseRandom");
+    list.innerHTML = `Translate:  ${signUp}`;
+    text.innerHTML = `Result: ${chars}`;
+    time.innerHTML = `Runtime: ${data}ms`;
+    randomPhrase.innerHTML = `Phrase of the day: <span> ${random(
+      phrase
+    )} </span>`;
   } catch {
     text.innerHTML = "The number is smaller";
   }
