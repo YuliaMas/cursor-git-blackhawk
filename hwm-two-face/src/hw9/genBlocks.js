7dfef139a27dc3898241e5f04446e36eed47d974
Yuliya <yulia.masechko@gmail.com>
2020-09-02 13:11:41 +0300

    add new function

---
+++ hwm-two-face/src/hw9/genBlocks.js
@@ -0,0 +1,42 @@
+const container = document.getElementById("container");
+const randomColorBlocks = document.querySelector(".colorBlocks");
+const stopButton = document.querySelector(".stop");
+const setBlocks = document.querySelector(".newBlocks");
+let timerId;
+
+const setBg = (elem) => {
+  elem.style.backgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+const setSquareContainer = () => {
+  for (let i = 0; i < 25; i++) {
+    container.insertAdjacentHTML("beforeend", `<div class="square"></div>`);
+  }
+};
+
+const setRandomColor = () => {
+  const divSquares = document.querySelectorAll(".square");
+  divSquares.forEach((square) => setBg(square));
+};
+
+function stop() {
+  clearInterval(timerId);
+}
+
+export default function () {
+  setBlocks.addEventListener("click", () => {
+    setBlocks.setAttribute("disabled", "disabled");
+    setSquareContainer();
+  });
+
+  randomColorBlocks.addEventListener("click", () => {
+    randomColorBlocks.setAttribute("disabled", "disabled");
+    timerId = setInterval(setRandomColor, 1000);
+  });
+
+  stopButton.addEventListener("click", () => {
+    randomColorBlocks.removeAttribute("disabled");
+    `${stop()}`;
+  });
+}
