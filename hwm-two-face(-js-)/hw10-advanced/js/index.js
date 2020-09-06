c57410787c182da5e8fb665309bea24f140a8514
Yuliya <yulia.masechko@gmail.com>
2020-09-07 02:29:29 +0300

    change role buttons

--- hwm-two-face(-js-)/hw10-advanced/js/index.js
+++ hwm-two-face(-js-)/hw10-advanced/js/index.js
@@ -5,12 +5,27 @@ window.addEventListener("DOMContentLoaded", () => {
   const getBird = document.getElementById("bird");
   const getWashDishes = document.getElementById("washDish");
   const getLamp = document.getElementById("light");
+  const bellSound = document.getElementById("bellSound");
+  const knockSound = document.getElementById("knockSound");
+  const carSound = document.getElementById("carSound");
+  const washDishesSound = document.getElementById("washDishesSound");
+  const birdSound = document.getElementById("birdSound");
+  const lightLampSound = document.getElementById("lightLampSound");
+  const audio = [ bellSound, carSound , knockSound, washDishesSound,birdSound ,lightLampSound];
+
+  function stopSound() {
+  audio.map((elems) => {
+    // elems.paused = true;
+    // elems.currentTime = 0;
+    elems.muted = true;
+  })
+  }
 
   const setSound = (elem, elemId) => {
-    // document.getElementById(elemId).play();
     if (elem.paused === true) {
       elem.currentTime = 0;
       document.getElementById(elemId).play();
+      elem.muted = false;
     } else if (elem.paused === false) {
       document.getElementById(elemId).pause();
     }
@@ -44,62 +59,74 @@ window.addEventListener("DOMContentLoaded", () => {
 
   /*---------------------------------------Buttons------------------------------------*/
   getBell.addEventListener("click", () => {
+    stopSound();
     const bellSound = document.getElementById("bellSound");
     setSound(bellSound, "bellSound");
   });
 
   getKnock.addEventListener("click", () => {
+    stopSound();
     const knockSound = document.getElementById("knockSound");
     setSound(knockSound, "knockSound");
   });
 
   getCar.addEventListener("click", () => {
+    stopSound();
     const carSound = document.getElementById("carSound");
     setSound(carSound, "carSound");
   });
 
   getBird.addEventListener("click", () => {
+    stopSound();
     const birdSound = document.getElementById("birdSound");
     setSound(birdSound, "birdSound");
   });
 
   getWashDishes.addEventListener("click", () => {
+    stopSound();
     const washDishesSound = document.getElementById("washDishesSound");
     setSound(washDishesSound, "washDishesSound");
   });
 
   getLamp.addEventListener("click", () => {
+    stopSound();
     const lightLampSound = document.getElementById("lightLampSound");
     setSound(lightLampSound, "lightLampSound");
   });
 
   document.addEventListener("keydown", (event) => {
     if (event.code === "KeyB") {
+      stopSound();
       const bellSound = document.getElementById("bellSound");
       setSound(bellSound, "bellSound");
       setColorBell(getBell);
     }
     if (event.code === "KeyK") {
+      stopSound();
       const knockSound = document.getElementById("knockSound");
       setSound(knockSound, "knockSound");
       setColorButton(getKnock);
     }
     if (event.code === "KeyC") {
+      stopSound();
       const carSound = document.getElementById("carSound");
       setSound(carSound, "carSound");
       setLastColor(getCar);
     }
     if (event.code === "KeyD") {
+      stopSound();
       const birdSound = document.getElementById("birdSound");
       setSound(birdSound, "birdSound");
       setColorButton(getBird);
     }
     if (event.code === "KeyW") {
+      stopSound();
       const washDishesSound = document.getElementById("washDishesSound");
       setSound(washDishesSound, "washDishesSound");
       setColorButton(getWashDishes);
     }
     if (event.code === "KeyL") {
+      stopSound();
       const lightLampSound = document.getElementById("lightLampSound");
       setSound(lightLampSound, "lightLampSound");
       setLightLamp(getLamp);
@@ -128,5 +155,5 @@ window.addEventListener("DOMContentLoaded", () => {
         setLampDefault(getLamp);
         break;
     }
-  });
-});
+  })
+})
